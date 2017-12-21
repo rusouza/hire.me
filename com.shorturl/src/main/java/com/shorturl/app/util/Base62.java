40749da9af72c02c741c0208d6918baf76194c30
spykerug <spykerug@gmail.com>
2017-12-21 00:53:58 -0200

    commit inicial

---
+++ com.shorturl/src/main/java/com/shorturl/app/util/Base62.java
@@ -0,0 +1,16 @@
+package com.shorturl.app.util;
+
+public class Base62 {
+	
+	public static final String ALFABETO = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+	public static final int BASE62 = ALFABETO.length();
+	
+	public static String paraBase62(long value) {
+        final StringBuilder sb = new StringBuilder(1);
+        do {
+            sb.insert(0, ALFABETO.charAt((int) (value % BASE62)));
+            value /= BASE62;
+        } while (value > 0);
+        return sb.toString();
+    }
+}
